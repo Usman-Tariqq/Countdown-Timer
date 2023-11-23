379f0a14e064addc9d134c54ae8d879da6ee1813
Usman-Tariqq <usmantariq2k5@gmail.com>
2023-11-23 11:38:41 +0500

    Updated

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const endDate = "19 November 2023 10:00 PM"
+const endDate = "24 November 2023 10:00 PM";
+const inputs = document.querySelectorAll("input");
 
-document.getElementById('end-date').innerText = endDate; 
-const inputs = document.querySelectorAll("input")
-
-function clock() {
-    const end = new Date (endDate)
-    const now = new Date ()
+function updateCountdown() {
+    const end = new Date(endDate);
+    const now = new Date();
     const diff = end - now;
-    console.log(diff);
-    inputs[0].value = Math.floor(diff / 3600 / 24);
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    inputs[0].value = days;
+    inputs[1].value = hours;
+    inputs[2].value = minutes;
+    inputs[3].value = seconds;
 }
-clock()
+
+updateCountdown(); // Initial call to set the countdown on page load
+setInterval(updateCountdown, 1000); // Update the countdown every second
